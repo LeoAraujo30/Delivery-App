484ea650ccf0b0bc93a715f413ee95ca390350fc
Lucas Carvalho <drigerm@hotmail.com>
2023-01-23 16:36:50 -0300

    primeiro req agora vai

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,13 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import Provider from './utils/Provider';
 
-ReactDOM.render((
+ReactDOM.render(
   <BrowserRouter>
     <Provider>
       <App />
     </Provider>
-  </BrowserRouter>, document.getElementById('root')
-));
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
