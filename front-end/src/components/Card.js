96432731f40bb1d9c4d19aae96c2a80db48b62ed
Marcos Schettino <schettinomarcosb@gmail.com>
2023-01-26 17:11:08 -0300

    fix

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,40 +1,89 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import CartContext from '../store/Cart.context';
 
-export default function CardProduct({ price, urlImage, name, index }) {
+export default function CardProduct({ price, urlImage, name, id, showItens }) {
+  const [quantity, setQuantity] = useState(0);
+  const { cart, setCart } = useContext(CartContext);
+  const cartProduct = {
+    id,
+    name,
+    price,
+    quantity: Number(quantity),
+    subtotal: (price * quantity).toFixed(2),
+  };
+
+  if (cartProduct > 0) {
+    showItens.push(cartProduct);
+  }
+
+  const handleChange = (event) => {
+    if (event.target.value <= 0) {
+      setQuantity(null);
+    } else {
+      setQuantity(event.target.value);
+    }
+  };
+
+  const addProduct = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const removeProduct = () => {
+    if (quantity <= 0) {
+      setQuantity(0);
+    } else {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  useEffect(() => {
+    const finalCart = cart.filter((prod) => prod.name !== name)
+      .filter((prod) => prod.quantity !== 0);
+    if (cartProduct.quantity <= 0) {
+      setCart([...finalCart]);
+    } else {
+      setCart([...finalCart, cartProduct]);
+    }
+  }, [quantity]);
+
   return (
     <div>
-      <h2 data-testid={ `customer_products__element-card-title-${index}` }>
+      <h2 data-testid={ `customer_products__element-card-title-${id}` }>
         { name }
       </h2>
       <h2
-        data-testid={ `customer_products__element-card-price-${index}` }
+        data-testid={ `customer_products__element-card-price-${id}` }
       >
-        { price }
+        { price.replace('.', ',') }
       </h2>
       <img
         height="100px"
         width="100px"
-        data-testid={ `customer_products__img-card-bg-image-${index}` }
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
         src={ urlImage }
         alt="Imagem do produto"
       />
       <div>
         <button
           type="button"
-          data-testid={ `customer_products__button-card-add-item-${index}` }
+          data-testid={ `customer_products__button-card-add-item-${id}` }
+          onClick={ addProduct }
         >
           +
         </button>
         <input
           type="number"
           min="0"
-          data-testid={ `customer_products__input-card-quantity-${index}` }
+          data-testid={ `customer_products__input-card-quantity-${id}` }
+          value={ quantity }
           placeholder="0"
+          onChange={ (event) => { handleChange(event); } }
         />
         <button
           type="button"
-          data-testid={ `customer_products__button-card-rm-item-${index}` }
+          data-testid={ `customer_products__button-card-rm-item-${id}` }
+          onClick={ removeProduct }
         >
           -
         </button>
@@ -44,8 +93,9 @@ export default function CardProduct({ price, urlImage, name, index }) {
 }
 
 CardProduct.propTypes = {
-  index: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
   price: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   urlImage: PropTypes.string.isRequired,
+  showItens: PropTypes.instanceOf(Array).isRequired,
 };
--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,10 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import fetchProducts from '../api/fetchProducts';
 import Card from '../components/Card';
 import Navbar from '../components/Navbar';
+import setLocalStorage from '../services/setLocalStorage';
+import CartContext from '../store/Cart.context';
 
 export default function Products() {
   const [products, setProducts] = useState([]);
+  const [disable, setDisable] = useState(true);
+  const [showTotalPrice, setShowTotalPrice] = useState(0);
+  const { cart } = useContext(CartContext);
+  const showItens = [];
+
+  useEffect(() => {
+    if (showTotalPrice === '0,00') {
+      setDisable(true);
+    } else {
+      setDisable(false);
+    }
+  }, [showTotalPrice]);
+
+  useEffect(() => {
+    const totalPrice = cart
+      .reduce((a, c) => a + Number(c.quantity * c.price), 0)
+      .toFixed(2);
+    const totalPriceCorrect = totalPrice.replace('.', ',');
+    setShowTotalPrice(totalPriceCorrect);
+  }, [cart]);
 
   useEffect(() => {
     fetchProducts(setProducts);
@@ -21,9 +44,26 @@ export default function Products() {
             price={ price }
             urlImage={ urlImage }
             name={ name }
+            showItens={ showItens }
           />
         ),
       )}
+      <Link
+        to="/customer/checkout"
+        data-testid="customer_products__checkout-bottom-value"
+      >
+        <button
+          disabled={ disable }
+          type="button"
+          data-testid="customer_products__button-cart"
+          onClick={ () => setLocalStorage('productsCart', cart) }
+        >
+          Ver carrinho: R$
+          {
+            cart.length === 0 ? '0,00' : showTotalPrice
+          }
+        </button>
+      </Link>
     </>
   );
 }
