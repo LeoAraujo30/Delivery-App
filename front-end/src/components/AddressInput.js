751e399677fa6b7a31f598ed1f651a675b881752
LeoAraujo30 <leonardo.araujo300301@gmail.com>
2023-01-26 15:12:56 -0300

    tabela pronta

---
+++ front-end/src/components/AddressInput.js
@@ -0,0 +1,85 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import TableCheckout from '../components/TableCheckout';
+import AppContext from '../utils/AppContext';
+
+function AddressInput() {
+  // const [isButtonDisable, setIsButtonDisable] = useState('');
+  // const [invalidRegister, setInvalidRegister] = useState(false);
+  // const { email, setEmail, password, setPassword,
+  //   username, setUsername } = useContext(AppContext);
+
+  const navigate = useNavigate();
+  // const handleClick = async (body) => {
+  //   const api = axios.create({
+  //     baseURL: 'http://localhost:3001/user',
+  //   });
+  //   try {
+  //     const { data } = await api.post('/register', body);
+  //     const userObj = { name: username, email, role: 'customer', token: data };
+  //     localStorage.setItem('user', JSON.stringify(userObj));
+  //     setInvalidRegister(false);
+  //     navigate(`/${userObj.role}/products`);
+  //   } catch (_error) {
+  //     setInvalidRegister(true);
+  //   }
+  // };
+
+  // const isValidEmail = (emailAddress) => /\S+@\S+\.\S+/.test(emailAddress);
+
+  useEffect(() => {
+    // if (isValidEmail(email)
+    //   && password.length >= MIN_LENGHT_PASS
+    //   && username.length >= MIN_LENGHT_NAME) {
+    //   setIsButtonDisable(false);
+    // } else {
+    // setIsButtonDisable(true);
+    // }
+  }, []);
+
+  return (
+    <div>
+      {/* <label htmlFor="name-input">
+        <input
+          className="name-input"
+          data-testid="common_register__input-name"
+          type="text"
+          placeholder="Name"
+          onChange={ ({ target }) => setUsername(target.value) }
+        />
+      </label>
+
+      <label htmlFor="email-input">
+        <input
+          className="email-input"
+          data-testid="common_register__input-email"
+          type="email"
+          placeholder="Email"
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+      </label>
+
+      <label htmlFor="password-input">
+        <input
+          className="password-input"
+          data-testid="common_register__input-password"
+          type="password"
+          placeholder="Password"
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
+      </label>
+
+      <button
+        data-testid="common_register__button-register"
+        type="button"
+        disabled={ isButtonDisable }
+        onClick={ () => handleClick({ name: username, email, password }) }
+      >
+        Cadastrar
+      </button> */}
+    </div>
+  );
+}
+
+export default AddressInput;
---
+++ front-end/src/components/TableCheckout.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react';
+
+function TableCheckout() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(JSON.parse(localStorage.getItem('carrinho')));
+  }, []);
+
+  useEffect(() => {}, [products]);
+
+  removeItemCart = (name) => {
+    const newProducts = products.filter((product) => product.name !== name);
+    localStorage.setItem('carrinho', JSON.stringify(newProducts));
+    setProducts(newProducts);
+  };
+
+  totalPrice = () => {
+    const result = products
+      .reduce((acc, product) => acc + (product.value * product.quantity), 0);
+    return result;
+  };
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Item</th>
+            <th scope="col">Descrição</th>
+            <th scope="col">Quantidade</th>
+            <th scope="col">Valor Unitário</th>
+            <th scope="col">Sub-total</th>
+            <th scope="col">Remover Item</th>
+          </tr>
+        </thead>
+        <tbody>
+          { products.map((product, index) => {
+            const { name, quantity, value } = product;
+            return (
+              <tr key={ index }>
+                <td
+                  data-testid={
+                    `customer_checkout__element-order-table-item-number-${index}`
+                  }
+                >
+                  {index + 1}
+                </td>
+                <td
+                  data-testid={
+                    `customer_checkout__element-order-table-name-${index}`
+                  }
+                >
+                  {name}
+                </td>
+                <td
+                  data-testid={
+                    `customer_checkout__element-order-table-quantity-${index}`
+                  }
+                >
+                  {quantity}
+                </td>
+                <td
+                  data-testid={
+                    `customer_checkout__element-order-table-unit-price-${index}`
+                  }
+                >
+                  {`R$ ${value}`}
+                </td>
+                <td
+                  data-testid={
+                    `customer_checkout__element-order-table-sub-total-${index}`
+                  }
+                >
+                  {`R$ ${(value * quantity).toFixed(2)}`}
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    data-testid={
+                      `customer_checkout__element-order-table-remove-${index}`
+                    }
+                    onClick={ () => removeItemCart(name) }
+                  >
+                    Remover
+                  </button>
+                </td>
+              </tr>
+            );
+          }) }
+        </tbody>
+      </table>
+      <h2 data-testid="customer_checkout__element-order-total-price">
+        {`Total: R$ ${totalPrice().toFixed(2)}`}
+      </h2>
+    </div>
+  );
+}
+
+export default TableCheckout;
