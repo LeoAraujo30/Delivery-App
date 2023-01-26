0ac593c5269f98abdde564dc5b53c5d78b66d391
Marcos Schettino <schettinomarcosb@gmail.com>
2023-01-26 17:42:55 -0300

    try 19284619846198

--- back-end/src/api/Routes/routes.js
+++ back-end/src/api/Routes/routes.js
@@ -7,6 +7,6 @@ const routes = Router();
 
 routes.use('/user', userRouter);
 routes.use('/sale', saleRouter);
-routes.use('/products', productsRouter);
+routes.use('/customer/products', productsRouter);
 
 module.exports = routes;
