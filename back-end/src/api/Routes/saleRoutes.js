d3329fa5a7484d61bb9d4557d887ebc07d3509d0
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-01-31 20:04:45 -0300

    feito rotas GET /user/seller/:sellerId/sales e PUT /sale/status

--- back-end/src/api/Routes/saleRoutes.js
+++ back-end/src/api/Routes/saleRoutes.js
@@ -7,4 +7,6 @@ saleRouter.post('/register', saleController.registerSale);
 
 saleRouter.get('/:saleId', saleController.saleDetails);
 
+saleRouter.put('/status', saleController.updateSaleStatus);
+
 module.exports = saleRouter;
